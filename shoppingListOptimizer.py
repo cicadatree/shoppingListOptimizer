35ef52639076941a13b56a2155bae08db810ee3b
Brendan Knowles <me@brendanknowles.com>
2021-04-20 20:34:16 -0400

    fixed shoppingDct sort function, kind of

--- shoppingListOptimizer.py
+++ shoppingListOptimizer.py
@@ -1,29 +1,35 @@
 from MasterIndex import masterIndexDict
 
-shoppingList = []
+shoppingLst = []
 quitShop = False
 
-# NOT DONE: function that sorts items in shoppingList so that items are optimized based on store layout
-def storeSort(shoppingList):
+#define a function to convert shoppingLst into a dictionary
+def lstToDctConvert(lst):
+    shoppingDct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
+    return shoppingDct
+
+lstToDctConvert(shoppingLst)
+
+# NOT DONE: function that sorts items in shoppingLst so that items are optimized based on store layout
+def storeSort(shoppingLst):
 
     #iterate through the new shopping list dictionary, and check if each key is the same (or exists) compared to the masterIndexDict
-    shoppingDict = enumerate(shoppingList)
-    for key in shoppingDict:
+    for key in shoppingLst:
         match = True
-        if not key in masterIndexDict or shoppingDict[key] != masterIndexDict[key]:
+        if not key in masterIndexDict or shoppingLst[key] != masterIndexDict[key]:
             match = False
     return 
 
 while quitShop == False:
 
-    #add the user's inputted shopping list item to shoppingList
+    #add the user's inputted shopping list item to shoppingLst
     addItem = input("Please enter your shopping list items here (when finished, enter DONE): ")
-    shoppingList.append(addItem)
+    shoppingLst.append(addItem)
 
     #break the loop if the user inputs "DONE"
     if addItem == "DONE":
-        storeSort(shoppingList)
+        storeSort(shoppingLst)
         print("Your shopping list contains: ")
-        for items in shoppingList[:-1]:
+        for items in shoppingLst[:-1]:
             print(items)
         break 
