879f7e8e6d30906584098fb60058bbba7a238b96
Brendan Knowles <me@brendanknowles.com>
2021-05-01 21:44:10 -0400

    added Trello task list to track progress in new features and optimization

--- shoppingListOptimizer.py
+++ shoppingListOptimizer.py
@@ -1,6 +1,7 @@
+# Trello task list: https://trello.com/c/eCT1YtjH/3-create-index-dictionary-for-sobeys-bridgeport
+
 from MasterIndex import *
 
-#shoppingLst is currently has placeholder values for testing. It should normally be empty when initialized.
 shoppingLst = ["Buns", "Milk", "Apples", "Pork Chops"]
 
 print(shoppingLst)
@@ -9,7 +10,6 @@ def fitness(item):
     return categoryOrderDict[itemCategoryDict[item]]
 
 while True:
-
     #add the user's inputted shopping list item to shoppingLst
     addItem = input("Please enter your shopping list items here (when finished, enter DONE): ")
     
@@ -22,32 +22,13 @@ while True:
 
             if itemCategory not in categoryOrderDict:
                 print(list(enumerate(categoryOrderList)))
-                categoryOrder = input("No order exists for this category. Please enter the order number for the category: ")
+                
                 categoryOrderList.insert(int(categoryOrder), itemCategory)
                 categoryOrderDict = {x: i for i, x in enumerate(categoryOrderList)}
                 print(categoryOrderDict)
     else:
         print("Your shopping list contains: ")
-
         shoppingLst.sort(key=fitness)
         for items in shoppingLst:
             print(items)
         break 
-        
-
-
-'''# NOT DONE: function that sorts items in shoppingLst so that items are optimized based on store layout
-def storeSort(shoppingLst):
-
-   #convert shoppingLst into a dictionary using lstToDctConvert()
-    lstToDctConvert(shoppingLst)
-
-    #just checking that the dict converter actually works (it does)
-    print(shoppingLst)
-
-    #iterate through the new shopping list dictionary, and check if each key is the same (or exists) compared to the masterIndexDict
-    for key in shoppingLst:
-        match = True
-        if not key in masterIndexDct:
-            match = False
-    return '''
